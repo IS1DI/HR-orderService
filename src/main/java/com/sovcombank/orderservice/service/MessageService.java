420852792d9909c3179fd4a97d6a5ee18ecf4867
IS1DI <slavaivankov236@gmail.com>
2023-08-26 02:42:44 +0300

    fixed messages

--- src/main/java/com/sovcombank/orderservice/service/MessageService.java
+++ src/main/java/com/sovcombank/orderservice/service/MessageService.java
@@ -5,18 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
-
 import java.util.Locale;
 
 @Service
 public class MessageService {
-    private static final String ORDER_PREFIX = "order.";
 
     @Autowired
     MessageSource messages;
 
-    public String orderMessage(MessageMethod method, Object[] args, Locale locale){
-        return messages.getMessage((ORDER_PREFIX + method.getVal()).intern(), args,locale);
+    public String getMessage(MessageMethod method, Object[] args, Locale locale){
+        return messages.getMessage(method.getVal(), args,locale);
     }
     public String accessErrorMessage(Locale locale){
         return messages.getMessage(MessageMethod.ACCESS_ERROR.getVal(),null,locale);
